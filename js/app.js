18ab3f926336f60affddc1c2994bd26594e9e9c1
allenarango <ararango@Allens-MacBook-Air.local>
2014-07-20 23:52:58 -0500

    Finished!

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 	var i = 0;
 	for (i = 1; i < 101; i ++) {
-	if (i % 3 == 0) {
+	if (i % 5 === 0 && i % 3 === 0) {
+		$(".number").append('<p>' + "FizzBuzz" + '</p>');
+	}
+	else if (i % 3 === 0) {
 		$(".number").append('<p>' + "Fizz" + '</p>');
 	}
-	if (i % 5 == 0) {
+	else if (i % 5 === 0) {
 		$(".number").append('<p>' + "Buzz" + '</p>');
-	}
-	if (i % 5 == 0, i % 3 == 0) {
-		$(".number").append('<p>' + "FizzBuzz" + '</p>');
 	}
 	else {
 		$(".number").append('<p>' + i + '</p>');
