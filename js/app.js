ba1347e9a035a939f2a66fa56626d6b41d57ba18
allenarango <ararango@Allens-MacBook-Air.local>
2014-07-27 15:44:58 -0500

    FizzBuzz Refractor

--- js/app.js
+++ js/app.js
@@ -1,17 +1,29 @@
 $(document).ready(function() {
-	var i = 0;
-	for (i = 1; i < 101; i ++) {
-	if (i % 5 === 0 && i % 3 === 0) {
-		$(".number").append('<p>' + "FizzBuzz" + '</p>');
-	}
-	else if (i % 3 === 0) {
-		$(".number").append('<p>' + "Fizz" + '</p>');
-	}
-	else if (i % 5 === 0) {
-		$(".number").append('<p>' + "Buzz" + '</p>');
-	}
-	else {
-		$(".number").append('<p>' + i + '</p>');
+
+var item = prompt ("Enter a number");
+	while(isNaN(item) === true) {
+			item = prompt ("You must enter a number")
+		}
+
+var i = 0;
+
+var game = function(number) {
+	for (var i = 1; i <= number; i ++) {
+		if (i % 5 === 0 && i % 3 === 0) {
+			$(".number").append('<p>' + "FizzBuzz" + '</p>');
+		}
+		else if (i % 3 === 0) {
+			$(".number").append('<p>' + "Fizz" + '</p>');
+		}
+		else if (i % 5 === 0) {
+			$(".number").append('<p>' + "Buzz" + '</p>');
+		}
+		else {
+			$(".number").append('<p>' + i + '</p>');
+		}
 	}
 }
+
+game (item);
+
 });
